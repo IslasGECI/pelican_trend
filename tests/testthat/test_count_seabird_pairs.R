c4f97a4b744419972710d76b65a59400e4a89056
Rocio Avendaño <rocio.avenvi.360@gmail.com>
2024-10-21 13:59:03 -0700

    change library name

--- tests/testthat/test_count_seabird_pairs.R
+++ tests/testthat/test_count_seabird_pairs.R
@@ -1,7 +1,7 @@
 describe("Get version of the module", {
-  it("The version is 0.1.0", {
-    expected_version <- c("0.1.0")
-    obtained_version <- packageVersion("templater")
+  it("The version is 0.0.1", {
+    expected_version <- c("0.0.1")
+    obtained_version <- packageVersion("pelicanr")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
   })
